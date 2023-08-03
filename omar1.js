3916bef6951baec330b9d331eb559daea9b665e5
Omar-Zantout <omar82199@gmail.com>
2023-08-03 08:04:41 +0300

    event delegation

--- omar1.js
+++ omar1.js
@@ -1,11 +1,27 @@
-let a = 3;
-var b = 4;
+// let a = 3;
+// var b = 4;
 
-function test(a, b) {
-  console.log(a * b);
-  var b = 8;
-  console.log(a * b);
-  let a = 6;
-  console.log(a * b);
+// function test(a, b) {
+//   console.log(a * b);
+//   var b = 8;
+//   console.log(a * b);
+//   let a = 6;
+//   console.log(a * b);
   
-}
+// }
+
+const myList = document.getElementById('myList');
+myList.addEventListener('click',function(e){
+  // console.log(e);
+  const  target = e.target;
+  if (target.matches("li")){
+    target.style.backgroundColor = red;
+
+  }
+
+
+  // add li
+  const newli = document.createElement("li");
+  newli.textContent = "dcode";
+  myList.appendChild(newli);
+});
